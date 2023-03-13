0e083140d85cb431903386e47dd2b28f350916b6
Hassan Kazi <andaijja@gmail.com>
2023-03-13 13:32:22 -0400

    started backend

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+// require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+const { NotFoundError } = require('./expressError');
+// const { authenticateJWT } = require("./middleware/auth");
+
+const app = express();
+
+// const userRoutes = require("./routes/user");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(authenticateJWT);
+
+// app.use("/user", userRoutes);
+// app.use("/auth", authRoutes);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
