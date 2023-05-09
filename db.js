4a402d2c6e1253eb22d970195e0349f7bf1065ec
Hassan Kazi <andaijja@gmail.com>
2023-05-09 15:02:55 -0400

    heroku db connect

--- db.js
+++ db.js
@@ -3,14 +3,24 @@ const dotenv = require('dotenv')
 
 let DB_URI;
 
-if (process.env.NODE_ENV === "test") {
-    DB_URI = "postgresql:///chatapp_test";
-} else {
-    DB_URI = process.env.DATABASE_URL || "postgresql:///chatapp";
-}
+// if (process.env.NODE_ENV === "test") {
+//     DB_URI = "postgresql:///chatapp_test";
+// } else {
+//     DB_URI = process.env.DATABASE_URL || "postgresql:///chatapp";
+// }
 
-let db = new Client({
-    connectionString: DB_URI
+// let db = new Client({
+//     connectionString: DB_URI
+// });
+
+// db.connect();
+
+
+const db = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect();
