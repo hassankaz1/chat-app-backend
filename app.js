b416e1500b0d8a0c391868379db9a62f59d1614b
Hassan Kazi <andaijja@gmail.com>
2023-05-10 15:33:56 -0400

    d

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const { NotFoundError } = require('./expressError');
 const { authenticateJWT } = require("./middleware/auth");
-const bodyParser = require('body-parser');
+
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(authenticateJWT);
-app.use(bodyParser({ limit: '50mb' }));
+app.options('*', cors())
 
 app.use("/user", userRoutes);
 app.use("/auth", authRoutes);
